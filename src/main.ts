d235dafd88e79d3211d22cf5bee49d215211ba6d
Angel Figuera <angeleraser@gmail.com>
2023-08-28 14:20:21 -0400

    refactor: remove container const

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-const container = document.getElementById("container") as HTMLDivElement;
-
 window.addEventListener("keyup", function ({ keyCode, key, code }) {
-  Object.entries({ keyCode, key: key.trim() || code, code }).forEach(
-    ([key, value]) => {
-      const kbdEl = document.querySelector(`[data-event="${key}"]`);
-      kbdEl!.textContent = value as string;
+  Object.entries({ keyCode, key: key.trim() || code, code }).forEach(([key, value]) => {
+      document.querySelector(`[data-event="${key}"]`)!.textContent = value as string;
     }
   );
 });
